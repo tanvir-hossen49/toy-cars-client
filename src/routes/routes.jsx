aeeb1628e9247ab1ca0b17951a571f54dba74c66
tanvir-hossen49 <thossen49@gmail.com>
2023-05-18 20:57:13 +0600

    add social media login system

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../page/Home/Home";
 import { SingIn } from "../page/SignIn.jsx/SignIn";
+import { SingUp } from "../page/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "/signin",
         element: <SingIn />,
+      },
+      {
+        path: "/signUp",
+        element: <SingUp />,
       },
     ],
   },
